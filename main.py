8d9700ce892d3261b7f916fd973292a07e2e4873
redroze <mecodie3@gmail.com>
2025-09-15 00:18:56 +0100

    Create Classes

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
+
+class Hotel:
+    def __init__(self,id):
+        pass
+    def book(self):
+       pass
+    def available(self):
+        pass
+
+class ReservationConfirm:
+    def __init__(self,customer_name,hotel_object):
+        pass
+
+    def generate(self):
+        pass
+
+print(df)
+id = input("Enter the id of the hotel:")
+hotel= Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name:")
+    reservationConfirm = ReservationConfirm(name,hotel)
+    reservationConfirm.generate()
+else:
+    print("Hotel is not free.")
